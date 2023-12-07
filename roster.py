a79b9c25c3f49b548904709aec63950e422e30be
Eleni Kafexhiu <elenik@unc.edu>
2023-12-07 17:35:26 -0500

    roster updated player list

--- roster.py
+++ roster.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-player = {"Last Name": ["Bacot", "Davis", "Cadeau"],
-          "First Name": ["Armando","RJ", "Elliot"],
-          "Height": [83,72,73],
-          "Weight": [240,180,180]
+player = {"Last Name": ["Bacot", "Davis", "Cadeau", "High", "Ryan", "Trimble", "Wojcik", "Washington", "Lebo", "Landry", "Withers", "James", "Farris", "Ingram"],
+          "First Name": ["Armando", "RJ", "Elliot", "Zayden", "Cormac", "Seth", "Paxson", "Jalen", "Creighton", "Rob", "JaeLyn", "Okonkwo", "Duwe", "Harrison"],
+          "Height": [83, 72, 73, 81, 77, 75, 77, 82, 73, 76, 81, 80, 79, 79],
+          "Weight": [240, 180, 180, 225, 195, 195, 195, 230, 180, 190, 215, 240, 210, 235]
           }
 data = pd.DataFrame(player)
 
